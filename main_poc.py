8614095219c95aad22f8f45470fa8df710d5172b
Miura0453 <marioaloc@ciencias.unam.mx>
2021-12-04 10:42:51 -0600

    Hola mundo

---
+++ main_poc.py
@@ -0,0 +1,14 @@
+
+
+# This .py file will be main invoker
+
+def _main_function(x):
+    # this is the main function
+    print(f'How are you {x}?')
+
+
+
+
+
+if __name__ == '__main__':
+    _main_function('Mario')
